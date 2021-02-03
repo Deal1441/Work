ab19cf20f743e88284954455266b0a8de8924371
Diana <di.rylnikova@yandex.ru>
2021-02-03 22:16:52 +0300

    Программа для определения количетсва цифр в получаемом числе

---
+++ DZ4/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    long long int n = 0;
+    long long int i = 0;
+    printf ("Please enter a number: ");
+    scanf ("%lli", &n);
+    while(n != 0)
+     {
+        n /= 10;
+        i++;
+      }
+
+    printf ("Number of digits ");
+    printf ("%lli\n", i);
+    return 0;
+}
