e0260ef214a68592fd1afdc22bb4b52ccd5cee09
Diana <di.rylnikova@yandex.ru>
2021-02-04 01:04:13 +0300

    Перевод из Dec в Bin

---
+++ DZ7/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x = 0;
+    printf("Please enter a decimal number: ");
+    scanf("%d", &x);
+    int i = 1;
+    int bin = 0;
+    while(x > 0)
+     {
+       bin += (x % 2) * i;
+       x = x / 2;
+       i *= 10;
+     }
+     printf("Binary number: ");
+     printf("%d", bin);
+     return 0;
+}
