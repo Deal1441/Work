646c8d62f400d5d923d79ddbfc27ac269249c4e3
Diana <di.rylnikova@yandex.ru>
2021-02-06 22:05:14 +0300

    Переписала задание по переводу из Dec в Bin

--- DZ7/main.cpp
+++ DZ7/main.cpp
@@ -2,17 +2,19 @@
 int main()
 {
     int x = 0;
-    printf("Please enter a decimal number: ");
-    scanf("%d", &x);
+    printf( "Please enter a decimal number: " );
+    scanf( "%d", &x );
+
     int i = 1;
     int bin = 0;
     while(x > 0)
-     {
-       bin += (x % 2) * i;
-       x = x / 2;
-       i *= 10;
+    {
+      bin += (x % 2) * i;
+      x >>= 1;;
+      i *= 10;
      }
-     printf("Binary number: ");
-     printf("%d", bin);
+
+     printf( "Binary number: " );
+     printf( "%d", bin );
      return 0;
 }
