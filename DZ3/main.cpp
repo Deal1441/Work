8ae37f3cc8e5915b9ad8df15da593a63391f4347
Diana <di.rylnikova@yandex.ru>
2021-02-06 21:44:30 +0300

    Красиво сделала третье задание

--- DZ3/main.cpp
+++ DZ3/main.cpp
@@ -2,21 +2,27 @@
 #include <math.h>
 
 int main(){
-    double r,a,d,aa,bb,ss;
-    printf("Введите r=");
-    scanf("%if", &r);
-    printf("Введите a=");
-    scanf("%if", &a);
-    printf("Введите d=");
-    scanf("%if", &d);
+    double r;
+    double a;
+    double d;
+    double aa;
+    double bb;
+    double ss;
+
+    printf("Enter r = ");
+    scanf( "%if", &r );
+    printf( "Enter a = " );
+    scanf( "%if", &a );
+    printf( "Enter d = " );
+    scanf( "%if", &d );
 
     aa=3.1416*r*r;
     bb=sqrt(3)/4*a*a;
     ss=d*d;
 
-
-    (aa>bb)?((aa>ss)?printf("Площадь круга больше\n")
-                   :printf("Площадь квадрата больше\n"))
-                   :((bb>ss)?printf("Площадь треугольника больше\n")
-                   :printf("Площадь квадрата больше\n"));
+    (aa>bb)?((aa>ss)?printf( " Square circle more \n" ) // Площадь круга больше
+                   :printf( " Square box more \n" ) )// Площадь квадрата больше
+                   :( ( bb>ss ) ? printf ( "Square triangle more \n" )
+                                  // Площадь треугольника больше
+                   : printf( "Square box more \n" ) );// Площадь квадрата больше
     }
