224c5e395e3f4b1f43c2b4b54c57e9934642d6eb
Diana <di.rylnikova@yandex.ru>
2021-02-08 22:57:22 +0300

    Программа по определению является ли она правильным скобочным выражением

---
+++ DZ14/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int a = 0;
+  int s = 0;
+  int d = 0;
+  constexpr int SIZE = 1024;
+  char str[SIZE];
+  char str_result[SIZE];
+  printf("Enter a line:");
+  scanf( "%s", str );
+
+  for (int i = 0; i < 12; ++i)
+  {
+    if (str[i] == '(')
+  {
+     ++a;
+  }
+    if (str[i] == ')')
+  {
+     --a;
+  }
+    if (str[i] == '{')
+  {
+     ++s;
+  }
+    if (str[i] == '}')
+  {
+     --s;
+  }
+    if (str[i] == '[')
+  {
+     ++d;
+  }
+    if (str[i] == ']')
+  {
+     --d;
+  }
+    }
+     if (a == 0 && s == 0 && d == 0)
+  { printf("Yes");
+  }
+   else {
+   printf("No");
+   }
+  }
